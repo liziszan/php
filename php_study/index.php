9d7619a92921e3d24bc1222dba50e7828a057a38
= <=>
2013-12-11 12:39:15 +0800

    xxx

--- php_study/index.php
+++ php_study/index.php
@@ -1,4 +1,13 @@
-<?php
-echo "hello world";
-echo "come one"
-?>
+<html>
+<title>这是我的第一个网页</title>
+<head></head>
+<body>
+<b>横看成岭侧成峰</b>
+<br>
+<font color="red">远近高低各不同</font>
+<br>
+<font size="6">不识庐山真面目</font>
+<br>
+<font style="bgcolor:red">只缘身在此山中</font>
+</body>
+</html>
